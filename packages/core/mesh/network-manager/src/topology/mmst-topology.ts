ea9f372094e8c1951250656617e63c7d594afc85
Nathan A. Ferch <nf+github@marginal.net>
2023-11-17 14:17:29 -0500

    fix: rate limit MMSTopology changes

--- packages/core/mesh/network-manager/src/topology/mmst-topology.ts
+++ packages/core/mesh/network-manager/src/topology/mmst-topology.ts
@@ -10,6 +10,8 @@ import { log } from '@dxos/log';
 
 import { type SwarmController, type Topology } from './topology';
 
+const MIN_UPDATE_INTERVAL = 1000 * 10;
+
 export interface MMSTTopologyOptions {
   /**
    * Number of connections the peer will originate by itself.
@@ -36,6 +38,8 @@ export class MMSTTopology implements Topology {
 
   private _sampleCollected = false;
 
+  private _lastAction = new Date(0);
+
   constructor({ originateConnections = 2, maxPeers = 4, sampleSize = 10 }: MMSTTopologyOptions = {}) {
     this._originateConnections = originateConnections;
     this._maxPeers = maxPeers;
@@ -52,7 +56,7 @@ export class MMSTTopology implements Topology {
     const { connected, candidates } = this._controller.getState();
     // Run the algorithms if we have first candidates, ran it before, or have more connections than needed.
     if (this._sampleCollected || connected.length > this._maxPeers || candidates.length > 0) {
-      log('Running the algorithm.');
+      log.info('Running the algorithm.');
       this._sampleCollected = true;
       this._runAlgorithm();
     }
@@ -74,22 +78,40 @@ export class MMSTTopology implements Topology {
     invariant(this._controller, 'Not initialized');
     const { connected, candidates, ownPeerId } = this._controller.getState();
 
+    // TODO(nf): does this rate limiting/flap dampening logic belong here or in the SwarmController?
     if (connected.length > this._maxPeers) {
       // Disconnect extra peers.
+      log.info(`disconnect ${connected.length - this._maxPeers} peers.`);
       const sorted = sortByXorDistance(connected, ownPeerId)
         .reverse()
         .slice(0, this._maxPeers - connected.length);
-      for (const peer of sorted) {
-        log(`Disconnect ${peer}.`);
-        this._controller.disconnect(peer);
+      invariant(sorted.length === 0);
+
+      if (sorted.length > 0) {
+        if (Date.now() - this._lastAction.getTime() > MIN_UPDATE_INTERVAL) {
+          this._controller.disconnect(sorted[0]);
+          this._lastAction = new Date();
+        } else {
+          log.info('rate limited discconnect');
+        }
+      } else {
+        log.info('no candidates to disconnect');
       }
     } else if (connected.length < this._originateConnections) {
       // Connect new peers to reach desired quota.
+      log.info(`connect ${this._originateConnections - connected.length} peers.`);
       const sample = candidates.sort(() => Math.random() - 0.5).slice(0, this._sampleSize);
       const sorted = sortByXorDistance(sample, ownPeerId).slice(0, this._originateConnections - connected.length);
-      for (const peer of sorted) {
-        log(`Connect ${peer}.`);
-        this._controller.connect(peer);
+
+      if (sorted.length > 0) {
+        if (Date.now() - this._lastAction.getTime() > MIN_UPDATE_INTERVAL) {
+          this._controller.connect(sorted[0]);
+          this._lastAction = new Date();
+        } else {
+          log.info('rate limited connect');
+        }
+      } else {
+        log.info('no candidates to connect to');
       }
     }
   }
